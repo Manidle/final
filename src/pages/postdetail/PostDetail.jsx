78fd5ded740774d5a3a413c2efd1fb4835493f4d
Thovy <kingvely151113@gmail.com>
2022-09-08 17:41:39 +0900

    feat: post 가져오기 구현

--- src/pages/postdetail/PostDetail.jsx
+++ src/pages/postdetail/PostDetail.jsx
@@ -1,4 +1,4 @@
-import { Container, Card, CardActions, Button, CardContent, TextField } from '@mui/material'
+import { Container, Card, CardActions, Button, CardContent, TextField, Grid } from '@mui/material'
 import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 import { useState, useEffect } from 'react'
 import React from 'react'
@@ -7,13 +7,61 @@ import Header from '../../components/header/Header'
 
 const PostDetail = () => {
 
+    // 게시글 데이터 가져오기
+    const [postBoard, setPostBoard] = useState("")
+    const [postTitle, setPostTitle] = useState("")
+    const [postContents, setPostContents] = useState("")
+    const [postLikeCount, setPostLikeCount] = useState(0)
+    const [postUser, setPostUser] = useState("")
+    const [postStayList, setPostStayList] = useState([])
+    const [postAttractionList, setPostAttractionList] = useState([])
+    const [postRentCarList, setPostRentCarList] = useState([])
+    const [postTrainList, setPostTrainList] = useState([])
+
+    useEffect(()=>{
+        axios.get('http://localhost:8080/post/1')
+        .then((response)=>{
+            console.log(response.data);
+            setPostBoard(response.data.boardId);
+            setPostTitle(response.data.title);
+            setPostContents(response.data.contents);
+            setPostLikeCount(response.data.likeCount);
+            setPostUser(response.data.userId);
+            setPostRentCarList(response.data.postRentCarList);
+            setPostTrainList(response.data.postTrainList);
+            setPostStayList(response.data.postStayList);
+            setPostAttractionList(response.data.postAttractionList);
+        })
+        .catch(function(error){
+            if (error.response) {
+                // 요청이 이루어졌으며 서버가 2xx의 범위를 벗어나는 상태 코드로 응답했습니다.
+                console.log("첫번째 에러");
+                console.log(error.response.data);
+            }
+            else if (error.request) {
+                // 요청이 이루어 졌으나 응답을 받지 못했습니다.
+                // `error.request`는 브라우저의 XMLHttpRequest 인스턴스 또는
+                // Node.js의 http.ClientRequest 인스턴스입니다.
+                console.log("두번째 에러");
+                console.log(error.request);
+            }
+            else {
+                // 오류를 발생시킨 요청을 설정하는 중에 문제가 발생했습니다.
+                console.log("세번째 에러");
+            console.log('Error', error.message);
+            }
+            console.log(error.config);
+        })
+    }, [])
+
+
     // reply 쓰기
     const [replyContent, setReplyContent] = useState("");
 
     function replySubmit(){
         axios.get('http://localhost:8080/reply', {
             params:{
-                user:8,
+                user:1,
                 post:1,
                 contents:replyContent
             }
@@ -61,7 +109,7 @@ const PostDetail = () => {
         axios.get('http://localhost:8080/reply/post/1')
         .then((response)=>{
             setReply(response.data)
-            console.log(reply);
+            // console.log(reply);
         })
         .catch(function(error){
             if (error.response) {
@@ -141,6 +189,18 @@ const PostDetail = () => {
                 <div className="postDetailContainer">
                     게시글내용
                 </div>
+                <Grid>
+                    게시판: {postBoard}
+                    게시글 제목: {postTitle}
+                    게시글 내용: {postContents}
+                    게시글 좋아요 수: {postLikeCount}
+                    게시글 작성자: {postUser}
+                    게시글 숙소리스트: {postStayList}
+                    게시글 관광지리스트: {postAttractionList}
+                    게시글 렌트카리스트: {postRentCarList}
+                    게시글 기차리스트: {postTrainList}
+
+                </Grid>
 
 
                 <div className="replyContainer">
