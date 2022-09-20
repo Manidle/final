ed53a369dd0f67ab36ac494e09066774cb2b8b32
소울치킨 <soulfever01@naver.com>
2022-09-20 14:37:24 +0900

    feat : 동그란 박스 컴포넌트 생성

---
+++ src/pages/attractiondetail/RoundBox.jsx
@@ -0,0 +1,23 @@
+import { Box } from "@mui/system";
+import React from "react";
+
+const RoundBox = ({ children }) => {
+  return (
+    <Box
+      sx={{
+        display: "block",
+        backgroundColor: "#F2E2FC",
+        padding: "1rem",
+        minWidth: "10rem",
+        borderRadius: "6rem",
+        margin: "auto",
+        marginBottom: "1rem",
+        textAlign: "center",
+      }}
+    >
+      {children}
+    </Box>
+  );
+};
+
+export default RoundBox;
