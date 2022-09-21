0f1db19ffd6f32ec2ea77fc77851cb3802ac7f61
소울치킨 <soulfever01@naver.com>
2022-09-21 15:32:08 +0900

    feat : 로그인페이지 왼쪽 부분 UI 적용

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -6,14 +6,17 @@ import Footer from "../../components/footer/Footer";
 import {
   Button,
   Container,
+  Grid,
   IconButton,
   SvgIcon,
   TextField,
+  Typography,
 } from "@mui/material";
 import { useState, useEffect } from "react";
 
 import { Person } from "@mui/icons-material";
 import Header from "../../components/header/Header";
+import { Box } from "@mui/system";
 
 export default function Login() {
   //navigatge
@@ -59,75 +62,110 @@ export default function Login() {
   return (
     <Container maxWidth="lg">
       <Header />
-      <div className="loginBackground">
-        <div className="loginContainer">
-          <div className="loginWrapper">
-            <div className="loginTitleIcon">
-              <Person fontSize="large" color="disabled" />
-            </div>
-            <div className="loginTitleContainer">
-              <span className="loginTitle">Login</span>
-            </div>
-            <div className="topLine"></div>
-            <form className="loginForm">
-              <TextField
-                id="standard-basic"
-                label="아이디"
-                variant="standard"
-                margin="normal"
-                size="small"
-                onChange={(e) => {
-                  setUserId(e.target.value);
-                }}
-              />
-              <TextField
-                id="standard-basic"
-                label="비밀번호"
-                variant="standard"
-                margin="normal"
-                size="small"
-                type="password"
-                onChange={(e) => {
-                  setPassword(e.target.value);
-                }}
-              />
-              <Button
-                onClick={loginClick}
-                className="loginButton"
-                variant="contained"
-              >
-                로그인
-              </Button>
-              <div className="underLine"></div>
-              <Button
-                type="button"
-                className="loginRegisterButton"
-                onClick={handleRoute}
-              >
-                아직 가입하지 않으셨나요?
-              </Button>
-              <div className="otherLogin">
-                <IconButton className="google">
-                  <img
-                    src="https://upload.wikimedia.org/wikipedia/commons/0/09/IOS_Google_icon.png"
-                    height="40px"
-                    width="40px"
-                    alt="google"
+
+      <Grid container>
+        <Grid item xs={8}>
+          <Box
+            sx={{
+              position: "fixed",
+              top: "40vh",
+              left: "10vh",
+              display: "flex",
+              flexDirection: "column",
+            }}
+          >
+            <Typography
+              fontWeight="bolder"
+              fontSize="3.5rem"
+              fontColor="#3D3D3D"
+            >
+              Tell Me Your Trip!
+            </Typography>
+            <Typography fontSize="2rem" fontWeight="normal" fontColor="#3D3D3D">
+              PLANNERGRAM
+            </Typography>
+            <Typography
+              fontSize="1.5rem"
+              fontWeight="bold"
+              fontColor="whitesmoke"
+            >
+              travel community
+            </Typography>
+          </Box>
+        </Grid>
+        <Grid item xs={4}>
+          <div className="loginBackground">
+            <div className="loginContainer">
+              <div className="loginWrapper">
+                <div className="loginTitleIcon">
+                  <Person fontSize="large" color="disabled" />
+                </div>
+                <div className="loginTitleContainer">
+                  <span className="loginTitle">Login</span>
+                </div>
+                <div className="topLine"></div>
+                <form className="loginForm">
+                  <TextField
+                    id="standard-basic"
+                    label="아이디"
+                    variant="standard"
+                    margin="normal"
+                    size="small"
+                    onChange={(e) => {
+                      setUserId(e.target.value);
+                    }}
                   />
-                </IconButton>
-                <IconButton className="google">
-                  <img
-                    src="https://m.gelatofactory.co.kr/web/upload/img/m/ico-kakao.png"
-                    height="40px"
-                    width="40px"
-                    alt="kakao"
+                  <TextField
+                    id="standard-basic"
+                    label="비밀번호"
+                    variant="standard"
+                    margin="normal"
+                    size="small"
+                    type="password"
+                    onChange={(e) => {
+                      setPassword(e.target.value);
+                    }}
                   />
-                </IconButton>
+                  <Button
+                    onClick={loginClick}
+                    className="loginButton"
+                    variant="contained"
+                  >
+                    로그인
+                  </Button>
+                  <div className="underLine"></div>
+                  <Button
+                    type="button"
+                    className="loginRegisterButton"
+                    onClick={handleRoute}
+                  >
+                    아직 가입하지 않으셨나요?
+                  </Button>
+                  <div className="otherLogin">
+                    <IconButton className="google">
+                      <img
+                        src="https://upload.wikimedia.org/wikipedia/commons/0/09/IOS_Google_icon.png"
+                        height="40px"
+                        width="40px"
+                        alt="google"
+                      />
+                    </IconButton>
+                    <IconButton className="google">
+                      <img
+                        src="https://m.gelatofactory.co.kr/web/upload/img/m/ico-kakao.png"
+                        height="40px"
+                        width="40px"
+                        alt="kakao"
+                      />
+                    </IconButton>
+                  </div>
+                </form>
               </div>
-            </form>
+            </div>
           </div>
-        </div>
-      </div>
+        </Grid>
+      </Grid>
+
       <Footer />
     </Container>
   );
