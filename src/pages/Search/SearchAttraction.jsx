272bd8e9fc8bcf4206365151bc7f26ddc7fe5f28
Thovy <kingvely151113@gmail.com>
2022-09-22 03:26:45 +0900

    feat: attraction 검색 기능 구현
    약간 이상하게 구현했지만 구현되긴했습니다.

--- src/pages/Search/SearchAttraction.jsx
+++ src/pages/Search/SearchAttraction.jsx
@@ -1,10 +1,9 @@
-import { Box, Button, Container, createTheme, ListItem, ThemeProvider, Typography } from '@mui/material';
+import { Box, Button, Container, createTheme, ListItem, Stack, TextField, ThemeProvider, Typography } from '@mui/material';
 import axios from 'axios';
 import React, { useEffect } from 'react'
 import { useState } from 'react';
 import CategoryBar from '../../components/CategoryBar';
 import Header from '../../components/header/Header';
-import jwt_decode from 'jwt-decode';
 
 const SearchAttraction = () => {
     const theme = createTheme({
@@ -26,13 +25,6 @@ const SearchAttraction = () => {
 
     const [attractions, setAttractions] = useState([])
 
-    const [attractionAddress, setAttractionAddress] = useState('');
-    const [attractionAddressDetail, setAttractionAddressDetail] = useState('');
-    const [attractionLikeCount, setAttractionLikeCount] = useState(0);
-    const [attractionName, setAttractionName] = useState('');
-    const [attractionPrice, setAttractionPrice] = useState(0);
-    const [attractionDetailAddress, setAttractionDetailAddress] = useState('');
-
     function searchAllAttraction(){
         axios.get('http://localhost:8080/api/auth/v1/list/attraction',{
             headers: {
@@ -41,11 +33,8 @@ const SearchAttraction = () => {
             }
         })
         .then((response)=>{
-            setAttractionAddress(response.address);
-            console.log(attractionAddress);
             setAttractions(response.data);
-            console.log(attractions);
-            console.log(attractions[0].name);
+            console.log(response.data);
         }
         )
     }
@@ -54,34 +43,30 @@ const SearchAttraction = () => {
         searchAllAttraction();
     },[])
 
+    // 검색어
     const [searchAttraction, setSearchAttraction] = useState("")
 
     function searchFilterAttraction(){
-        axios.get('http://localhost:8080/api/filter/attraction/search',{
-                search:{searchAttraction}
-            },
-            {headers: {
-                'Authorization': `${localStorage.getItem('token')}`,
-                "Content-Type": "application/json; charset=UTF-8",
-            }
-        })
+        axios.get(`http://localhost:8080/api/filter/attraction/search?search=${searchAttraction}`)
         .then((response=>{
             setAttractions(response.data);
             console.log(searchAttraction);
-            console.log(response.data);
         }))
+        .catch(function(error){
+            console.log(error.response);
+            console.log(searchAttraction);
+        })
     }
-
-
-    const userData = jwt_decode(localStorage.getItem('token'));
 
   return (
     <ThemeProvider theme={theme}>
         <Container maxWidth='lg'>
             <Header/>
             <CategoryBar category='attraction'/>
-            <input onChange={(e)=>{setSearchAttraction(e.target.value)}} />
-            <Button onClick={()=>{searchFilterAttraction()}}>테스트</Button>
+            <Stack display='flex'>
+                <TextField onChange={(e)=>{setSearchAttraction(e.target.value)}} />
+                <Button onClick={()=>{searchFilterAttraction()}}>검색</Button>
+            </Stack>
             <Box>
                 {attractions.length === 0 ?
                     <Box>관광지가 없습니다.</Box> :
@@ -89,7 +74,7 @@ const SearchAttraction = () => {
                         <ListItem display='flex' justifyContent='space-between' key={attraction.attractionId} dense='true'>
                             <Typography>관광지 이름: {attraction.name}</Typography>
                             <Typography>관광지 지역: {attraction.address}</Typography>
-                            <Typography>좋아요 수: </Typography>
+                            <Typography>좋아요 수: {attraction.likeCount}</Typography>
                         </ListItem>
                     ))
                 }
