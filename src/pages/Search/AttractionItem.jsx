03182f21398967b7b51c8929764ff2e1580b3776
소울치킨 <soulfever01@naver.com>
2022-09-22 16:22:44 +0900

    feat : AttractionItem 꾸미기

--- src/pages/Search/AttractionItem.jsx
+++ src/pages/Search/AttractionItem.jsx
@@ -1,5 +1,6 @@
-import { ListItem, Typography } from "@mui/material";
+import { Grid, ListItem, Typography } from "@mui/material";
 import React from "react";
+import TextProperty from "./TextProperty";
 
 const AttractionItem = ({ attraction }) => {
   return (
@@ -9,9 +10,22 @@ const AttractionItem = ({ attraction }) => {
       key={attraction.attractionId}
       dense="true"
     >
-      <Typography>관광지 이름: {attraction.name}</Typography>
-      <Typography>관광지 지역: {attraction.address}</Typography>
-      <Typography>좋아요 수: {attraction.likeCount}</Typography>
+      <Grid
+        container
+        width="40rem"
+        display="flex"
+        justifyContent="space-between"
+      >
+        <Grid item xs={4}>
+          <TextProperty>{attraction.name}</TextProperty>
+        </Grid>
+        <Grid item xs={4}>
+          <TextProperty>{attraction.address}</TextProperty>{" "}
+        </Grid>
+        <Grid item xs={4}>
+          <TextProperty>{attraction.likeCount}</TextProperty>{" "}
+        </Grid>
+      </Grid>
     </ListItem>
   );
 };
