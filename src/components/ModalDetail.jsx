3a1845cf24f2aecc208b82d696e8c74301c682e6
Thovy <kingvely151113@gmail.com>
2022-09-24 13:39:55 +0900

    refactor: 프리티어 적용

--- src/components/ModalDetail.jsx
+++ src/components/ModalDetail.jsx
@@ -1,58 +1,61 @@
-import { Box, Button, createTheme, TextField, ThemeProvider, Typography } from '@mui/material'
-import React from 'react'
-import ModalAttractionResult from './modalSearch/ModalAttractionResult';
-import ModalRentcarResult from './modalSearch/ModalRentcarResult';
-import ModalStayResult from './modalSearch/ModalStayResult';
-import ModalTrainResult from './modalSearch/ModalTrainResult';
+import {
+  Box,
+  Button,
+  createTheme,
+  TextField,
+  ThemeProvider,
+  Typography,
+} from "@mui/material";
+import React, { useEffect } from "react";
+import { useState } from "react";
+import ModalAttractionResult from "./modalSearch/ModalAttractionResult";
+import ModalRentcarResult from "./modalSearch/ModalRentcarResult";
+import ModalStayResult from "./modalSearch/ModalStayResult";
+import ModalTrainResult from "./modalSearch/ModalTrainResult";
 
-const ModalDetail = ({currentCategory}) => {
-    const theme = createTheme({
-        palette: {
-            primary: {
-                // 가장 어두운 보라
-            main: '#52057B',
-            },
-            secondary: {
-                // 가장 밝은 보라
-            main: '#BC6FF1',
-            },
-            info:{
-                // 중간 보라
-                main: '#892CDC',
-            },
-        },
-    });
+const ModalDetail = ({ currentCategory }) => {
+  const theme = createTheme({
+    palette: {
+      primary: {
+        // 가장 어두운 보라
+        main: "#52057B",
+      },
+      secondary: {
+        // 가장 밝은 보라
+        main: "#BC6FF1",
+      },
+      info: {
+        // 중간 보라
+        main: "#892CDC",
+      },
+    },
+  });
 
   return (
     <ThemeProvider theme={theme}>
-        {currentCategory === 'stay' ?
-            <Box>
-                <Typography>stay</Typography>
-                <ModalStayResult />
-            </Box> :
-            (
-                currentCategory === 'attraction' ?
-                <Box >
-                    <Typography>attraction</Typography>
-                    <ModalAttractionResult/>
-                </Box> :
-                (
-                    currentCategory === 'train' ?
-                    <Box >
-                        <Typography>train</Typography>
-                        <ModalTrainResult/>
-                    </Box> :
-                    (
-                        <Box >
-                            <Typography>rentcar</Typography>
-                            <ModalRentcarResult/>
-                        </Box>
-                    )
-                )
-            )
-        }
+      {currentCategory === "stay" ? (
+        <Box>
+          <Typography>stay</Typography>
+          <ModalStayResult />
+        </Box>
+      ) : currentCategory === "attraction" ? (
+        <Box>
+          <Typography>attraction</Typography>
+          <ModalAttractionResult />
+        </Box>
+      ) : currentCategory === "train" ? (
+        <Box>
+          <Typography>train</Typography>
+          <ModalTrainResult />
+        </Box>
+      ) : (
+        <Box>
+          <Typography>rentcar</Typography>
+          <ModalRentcarResult />
+        </Box>
+      )}
     </ThemeProvider>
-  )
-}
+  );
+};
 
-export default ModalDetail
+export default ModalDetail;
