e5186acc39f24ac0bf1efdfe5c4e1308c510e1b6
Thovy <kingvely151113@gmail.com>
2022-09-25 03:20:56 +0900

    refactor: HotPosts 컴포넌트 추가

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,30 +1,26 @@
-import React from 'react'
-import Featured from '../../components/featured/Featured'
-import Searchbar from '../../components/searchbar/Searchbar'
-import HotPost from '../../components/hotpost/HotPost'
-import Footer from '../../components/footer/Footer'
-import { Container, createTheme, ThemeProvider } from '@mui/material'
-import Header from '../../components/header/Header'
-
-
+import React from "react";
+import Featured from "../../components/featured/Featured";
+import Searchbar from "../../components/searchbar/Searchbar";
+import HotPost from "../../components/hotpost/HotPost";
+import Footer from "../../components/footer/Footer";
+import { Container, createTheme, ThemeProvider } from "@mui/material";
+import Header from "../../components/header/Header";
+import HotPosts from "../../components/hotpost/HotPosts";
 
 const Home = () => {
-  const theme = createTheme
-  
-
+  const theme = createTheme;
 
   return (
-      <Container maxWidth="lg">
-        <Header/>
-        <div className="homeContainer">
-            <Featured/>
-            <Searchbar/>
-            <h1 className="homeTitle"></h1>
-            <HotPost/>
-        </div>
-        <Footer/>
-      </Container>
-  )
-}
+    <Container maxWidth="lg">
+      <Header />
+      <div className="homeContainer">
+        <Featured />
+        <HotPost />
+        <HotPosts />
+      </div>
+      <Footer />
+    </Container>
+  );
+};
 
-export default Home
+export default Home;
