40dc5ec8b5d4fe8f1129d0e7cdc1ce0f69855dcd
소울치킨 <soulfever01@naver.com>
2022-09-25 21:37:37 +0900

    feat : 슬라이드바 버튼 수정

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "./featured.css";
 import Carousel from "react-material-ui-carousel";
+import { Button } from "@mui/material";
+import { Box } from "@mui/system";
 
 const Featured = () => {
   // 캐러셀 이미지 모음
@@ -21,34 +23,52 @@ const Featured = () => {
       <div className="carouselContainer">
         <div className="carouselWrapper">
           <Carousel
+            // fullHeightHover={false} // We want the nav buttons wrapper to only be as big as the button element is
             indicators={true}
             animation="fade"
             autoPlay={true}
             navButtonsAlwaysVisible={true}
+            NavButton={({ onClick, next, prev }) => {
+              // Other logic
+
+              return (
+                <Button
+                  onClick={onClick}
+                  color="secondary"
+                  sx={{
+                    top: "50vh",
+
+                    marginLeft: "5rem",
+                    marginRight: "5rem",
+                    color: "#892CDC",
+                    fontSize: "0.8rem",
+                    width: "2rem",
+                    borderRadius: "0.6rem",
+                    cursor: "pointer",
+                    "&:hover": {
+                      opacity: "1",
+                    },
+                  }}
+                >
+                  {next && "Next"}
+                  {prev && "Prev"}
+                </Button>
+              );
+            }}
             sx={{
               paddingBottom: "5rem",
             }}
           >
-            {images.map((image, i) => (
-              <div
+            {images.map((image) => (
+              <img
+                src={image.src}
+                alt="이미지"
                 style={{
-                  width: "80%",
-                  height: "30vw",
-                  maxHeight: "400px",
-                  marginLeft: "10%",
-                  marginRight: "10%",
+                  objectFit: "cover",
+                  width: "100%",
+                  height: "50vh",
                 }}
-              >
-                <img
-                  src={image.src}
-                  style={{
-                    width: "100%",
-                    objectFit: "cover",
-                    minHeight: "200px",
-                    maxHeight: "400px",
-                  }}
-                />
-              </div>
+              />
             ))}
           </Carousel>
         </div>
