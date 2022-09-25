2c07ef566141668d141464f98eb1205af28c6097
Yiseun <yiseunn@gmail.com>
2022-09-25 17:50:57 +0900

    feat: home에 hotcomponent 추가

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,30 +1,32 @@
-import React from 'react'
-import Featured from '../../components/featured/Featured'
-import Searchbar from '../../components/searchbar/Searchbar'
-import HotPost from '../../components/hotpost/HotPost'
-import Footer from '../../components/footer/Footer'
-import { Container, createTheme, ThemeProvider } from '@mui/material'
-import Header from '../../components/header/Header'
-
-
+import React from "react";
+import Featured from "../../components/featured/Featured";
+import Searchbar from "../../components/searchbar/Searchbar";
+import HotPost from "../../components/hotpost/HotPost";
+import Footer from "../../components/footer/Footer";
+import { Container, createTheme, ThemeProvider } from "@mui/material";
+import Header from "../../components/header/Header";
+import HotArticle from "../../components/hotarticle/HotArticle";
+import HotAttraction from "../../components/hotattraction/HotAttraction";
 
 const Home = () => {
-  const theme = createTheme
-  
-
+  const theme = createTheme;
 
   return (
-      <Container maxWidth="lg">
-        <Header/>
-        <div className="homeContainer">
-            <Featured/>
-            <Searchbar/>
-            <h1 className="homeTitle"></h1>
-            <HotPost/>
-        </div>
-        <Footer/>
-      </Container>
-  )
-}
+    <Container maxWidth="lg">
+      <Header />
+      <div className="homeContainer">
+        <Featured />
+        <Searchbar />
+        <h1 className="homeTitle">start</h1>
+        <HotArticle />
+        <HotAttraction />
+        <h1 className="homeTitle">end</h1>
 
-export default Home
+        <HotPost />
+      </div>
+      <Footer />
+    </Container>
+  );
+};
+
+export default Home;
